4cc45072d3b1ebfc52fa2b829f173805b15d80bd
Garrett Bodley <garrett.bodley@gmail.com>
2023-12-03 18:25:36 -0500

    Another draft for part one. Doesn't work.

--- 03/ruby/one.rb
+++ 03/ruby/one.rb
@@ -14,37 +14,67 @@ log_file = File.open(log_path, File::CREAT | File::RDWR | File::TRUNC)
 
 lines = input_file.readlines(chomp: true)
 
-def partnum?(num, y, lines)
-  binding.pry if num == '161'
+def putsmatrix(matrix)
+  matrix.each { |row| puts row.join('') }
+end
+
+def partnum?(num, ay, lines)
   pattern = /[!@\#$%^&*()_+={}\[\]:;"'<>,?\/\-~`|\\]/
-  start = lines[y].match(num).begin(0)
-  finish = start + num.length
+  numstart = lines[ay].match(num).begin(0)
+  numfinish = numstart + num.length
 
-  x = start
+  matrix = []
 
-  # check first char
+  xstart = numstart > 0 ? numstart - 1 : numstart
+  xfinish = numfinish < lines[ay].length ? numfinish + 1 : numfinish
 
-  # check preceding above, inline, and below of first char
-  return true if x > 0 && y > 0 && lines[y - 1][x - 1].match?(pattern)
-  return true if x > 0 && lines[y][x - 1].match?(pattern)
-  return true if x > 0 && y < lines.length - 1 && lines[y + 1][x - 1].match?(pattern)
+  ystart = ay > 0 ? ay - 1 : ay
+  yfinish = ay < lines.length - 1 ? ay + 2 : ay + 1
 
-  # check above and below all chars
-
-  while x < finish
-    return true if y > 0 && lines[y - 1][x].match?(pattern)
-    return true if y < lines.length - 1 && lines[y + 1][x].match(pattern)
-
-    x += 1
+  # lets go line by line!
+  y = ystart
+  while y < yfinish
+    j = xstart
+    row = []
+    x = xstart
+    while x < xfinish
+      row << lines[y][x]
+      x += 1
+    end
+    matrix << row
+    y += 1
   end
+  putsmatrix(matrix)
 
-  # check postceding above, inline, and below of last char
+  # # check first char
 
-  return true if x < lines[y].length - 1 && y > 0 && lines[y - 1][x + 1].match?(pattern)
-  return true if x < lines[y].length - 1 && lines[y][x + 1].match?(pattern)
-  return true if x < lines[y].length - 1 && y < lines.length - 1 && lines[y + 1][x + 1].match?(pattern)
+  # # check preceding above, inline, and below of first char
+  # return true if x > 0 && y > 0 && lines[y - 1][x - 1].match?(pattern)
+  # return true if x > 0 && lines[y][x - 1].match?(pattern)
+  # return true if x > 0 && y < lines.length - 1 && lines[y + 1][x - 1].match?(pattern)
 
-  false
+  # # check above and below all chars
+
+  # while x < finish
+  #   return true if y > 0 && lines[y - 1][x].match?(pattern)
+  #   return true if y < lines.length - 1 && lines[y + 1][x].match(pattern)
+
+  #   x += 1
+  # end
+
+  # # check postceding above, inline, and below of last char
+
+  # return true if x < lines[y].length - 1 && y > 0 && lines[y - 1][x + 1].match?(pattern)
+  # return true if x < lines[y].length - 1 && lines[y][x + 1].match?(pattern)
+  # return true if x < lines[y].length - 1 && y < lines.length - 1 && lines[y + 1][x + 1].match?(pattern)
+
+  matrix_string = matrix.map{ |row| row.join('') }.join('')
+  valid = matrix_string.match?(pattern)
+  # binding.pry if numfinish == lines[y].length - 1 || numstart == 0
+  # binding.pry if ay == 0 || ay == lines.length - 1
+  # binding.pry if num == "169"
+
+  valid
 end
 
 sum = 0
@@ -53,9 +83,11 @@ lines.each_with_index do |line, linenum|
   # first lets find series of digits
   nums = line.scan(/\d+/)
   partnums = nums.select { |num| partnum?(num, linenum, lines) }
-  rejected = nums.reject { |num| partnum?(num, linenum, lines)}
+  rejected = nums.reject do |num|
+    partnum?(num, linenum, lines)
+  end
   partnums.each{ |num| sum += num.to_i }
-  log_file.puts "#{linenum}: #{rejected.join(' ')}"
+  log_file.puts "#{linenum}: #{rejected.join(' ')}" if rejected.count > 0
 end
 
 log_file.puts(sum)
