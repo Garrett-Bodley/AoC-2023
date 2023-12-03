d73d9d4d470d3e53c205a1f1c1c0483475f3f650
Garrett Bodley <garrett.bodley@gmail.com>
2023-12-03 16:34:41 -0500

    Ruby solution Day 2, Part 2, 2023

---
+++ 02/ruby/two.rb
@@ -0,0 +1,38 @@
+require 'pathname'
+require 'pry-nav'
+
+# https://adventofcode.com/2023/day/2
+
+base_path = Pathname.new(File.expand_path(__dir__))
+input_path = base_path.parent.join('input.txt')
+log_path = base_path.join('rlog2.txt')
+
+input_file = File.open(input_path, File::RDONLY)
+log_file = File.open(log_path, File::CREAT | File::RDWR | File::TRUNC)
+
+lines = input_file.readlines(chomp: true)
+sum = 0
+
+lines.each do |line|
+  hash = {}
+  valid = false
+  tokens = line.split(/ ?[:,;] ?/)
+  id = tokens[0].match(/\d+/)[0].to_i
+
+  tokens.slice(1..-1).each do |token|
+    count, color = token.split(/ /)
+    hash[color] = count.to_i unless hash.key?(color)
+    hash[color] = count.to_i if count.to_i > hash[color]
+  end
+
+  # valid = true if hash['red'] <= 12 && hash['green'] <= 13 && hash['blue'] <= 14
+  #
+  power = hash['red'] * hash['green'] * hash['blue']
+  sum += power
+
+  log_file.puts("Id: #{id}, sum: #{sum}, power: #{power}, red: #{hash['red']}, green: #{hash['green']}, blue:  #{hash['blue']}")
+end
+
+puts sum
+
+# expect 65371
