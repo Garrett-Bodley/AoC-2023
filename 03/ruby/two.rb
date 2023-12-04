119302ff18a7d956062bdc763a0c24502cff5b9d
Garrett Bodley <garrett.bodley@gmail.com>
2023-12-03 21:21:33 -0500

    Day Three Part Two Solution

--- 03/ruby/two.rb
+++ 03/ruby/two.rb
@@ -40,23 +40,50 @@ def partnum?(num, y, lines)
   false
 end
 
+def find_ratio(gear, geary, lines)
+  gearx = gear.begin(0)
+  xstart = gearx > 0 ? gearx - 1 : gearx
+  xend = gearx < lines[0].length - 1 ? gearx + 1 : gearx
+
+  ystart = geary > 0 ? geary - 1 : geary
+  yend = geary < lines.length - 1 ? geary + 1 : geary
+
+  y = ystart
+
+  neighbors = []
+
+  while y <= yend
+    line_nums = lines[y].enum_for(:scan, /\d+/).map { Regexp.last_match }
+
+    line_nums.select! do |num_match_data|
+      num_start = num_match_data.begin(0)
+      num_last = num_match_data.end(0) - 1
+      num_range = (num_start..num_last).to_a
+      num_range.any? { |x| (xstart..xend).cover?(x) }
+    end
+    # binding.pry
+    line_nums.each { |match| neighbors << match[0].to_i }
+    return neighbors[0] * neighbors[1] if neighbors.length > 1
+
+    y += 1
+  end
+  0
+end
+
 sum = 0
 lines.each_with_index do |line, linenum|
   # looking for numbers that are adjacent to other numbers
   # first lets find series of digits
-  nums = line.enum_for(:scan, /\d+/).map { Regexp.last_match }
+  gears = line.enum_for(:scan, /\*/).map { Regexp.last_match }
 
   # binding.pry
-  partnums = nums.select do |num|
-    partnum?(num, linenum, lines)
+  gears.each do |gear|
+    ratio = find_ratio(gear, linenum, lines)
+    log_file.puts "Gear on line: #{linenum}, ratio: #{ratio}" if ratio > 0
+    sum += ratio
   end
-  # binding.pry
-  # rejected = nums.reject { |num| partnum?(num, linenum, lines)}
-  partnums.each{ |num| sum += num[0].to_i }
-  # log_file.puts "#{linenum}: #{rejected.join(' ')}"
 end
 
-log_file.puts(sum)
 puts sum
 
-# expect 550064
+# expect 85010461
