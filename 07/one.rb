74f870fdffb3efcf84f580bf915a3b23dfb96b1e
Garrett Bodley <garrett.bodley@gmail.com>
2023-12-07 11:31:29 -0500

    Code Cleanup 07/one.rb

--- 07/one.rb
+++ 07/one.rb
@@ -17,27 +17,7 @@ else
   LOG_PATH = FILE_PATH.parent.join('log_input1.txt')
 end
 
-# Five of a kind, where all five cards have the same label: AAAAA
-# 7
-#
-# Four of a kind, where four cards have the same label and one card has a different label: AA8AA
-# 6
-#
-# Full house, where three cards have the same label, and the remaining two cards share a different label: 23332
-# 5
-#
-# Three of a kind, where three cards have the same label, and the remaining two cards are each different from any other card in the hand: TTT98
-# 4
-#
-# Two pair, where two cards share one label, two other cards share a second label, and the remaining card has a third label: 23432
-# 3
-#
-# One pair, where two cards share one label, and the other three cards have a different label from the pair and each other: A23A4
-# 2
-#
-# High card, where all cards' labels are distinct: 23456
-# 1
-
+# Represents a Hand of cards
 class Hand
   attr_reader :cards, :bid, :strength
 
@@ -65,7 +45,8 @@ class Hand
     score_cards
   end
 
-  def score_cards
+  # rubocop plz shut up
+  def score_cards # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/MethodLength
     cardmap = {}
     @cards.each_char do |card|
       if cardmap[card]
@@ -75,7 +56,7 @@ class Hand
       end
     end
 
-    sorted = cardmap.to_a.sort {|a, b| b[1] - a[1]}
+    sorted = cardmap.to_a.sort { |a, b| b[1] - a[1] }
 
     case sorted[0][1]
     when 5
@@ -91,18 +72,15 @@ class Hand
     end
   end
 
-  def self.all
-    @all
+  class << self
+    attr_reader :all
   end
 
-  def self.sort_hands
+  def self.sort_hands # rubocop:disable Metrics/MethodLength
     @all.sort! do |a, b|
       comparison = a.strength - b.strength
 
-      if comparison != 0
-        comparison
-      else
-        comparison = 0
+      if comparison.zero?
         5.times do |i|
           a_strength = @cardmap[a.cards[i]]
           b_strength = @cardmap[b.cards[i]]
@@ -111,8 +89,9 @@ class Hand
           comparison = a_strength - b_strength
           break
         end
-        comparison
       end
+
+      comparison
     end
   end
 end
