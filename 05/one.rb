bd7c1b2ab2d91fd1d0afc65dee5cc0fbdee968ce
Garrett Bodley <garrett.bodley@gmail.com>
2023-12-06 21:28:57 -0500

    Day 5 Part 1 Solution

--- 05/one.rb
+++ 05/one.rb
@@ -1,99 +1,69 @@
-# frozen_string_literal: true
-
-require 'pry-nav'
 require 'pathname'
+require 'pry'
 
+class Mapping
+  attr_reader :src, :dest
 
-input_path = Pathname.new(File.expand_path('./test.txt'))
-log_path = input_path.parent.join('test_log.txt')
-
-input_file = File.open(input_path, File::RDONLY)
-log_file = File.open(log_path, File::CREAT | File::RDWR | File::TRUNC)
-
-lines = input_file.readlines(chomp: true)
-chunks = lines.chunk(&:empty?).map { |_, group| group }.reject { |el| el[0].empty? }
-
-seeds = chunks[0][0].split(' ').slice(1..).map(&:to_i)
-
-class MapRange
-  attr_accessor :destination, :source, :range
-
-  def initialize(destination, source, range)
-    @destination = destination
-    @source = source
-    @source_range = (source..source + range - 1)
-    @destination_range = (destination..destination + range - 1)
-    @range = (0..range - 1)
-  end
-
-  def source_cover?(source)
-    @source_range.cover?(source)
-  end
-end
-
-class Map
-  attr_accessor :source_type, :dest_type, :hash
-
-  def initialize(source_type, dest_type)
-    @source_type = source_type
-    @dest_type = dest_type
+  def initialize(src, dest)
+    @src, @dest = [src, dest].map(&:to_sym)
     @ranges = []
   end
 
-  def add_range(destination, source, range)
-    @ranges << MapRange.new(destination, source, range)
+  def add_range(dest_start, src_start, length)
+    offset = dest_start - src_start
+    @ranges << [
+      (src_start...(src_start + length)),
+      offset
+    ]
   end
 
-  def lookup(source)
-    if (match = @ranges.select { |range| range.source_cover?(source) }[0]).nil?
-      source
+  def map(number)
+    if (range = @ranges.find { |s, _| s.cover?(number) })
+      src_range, offset = range
+      number + offset
     else
-      offset = source - match.source
-      match.destination + offset
+      number
     end
   end
 end
 
+almanac = []
+seeds = []
+
+File.open(File.join(__dir__, 'input.txt'), 'r') do |file|
+  line = file.readline.chomp
+  seeds = line[7..].split(/\s+/).map(&:to_i)
+  file.readline
+
+  until file.eof?
+    line = file.readline.chomp
+    _, source, destination = /(\w+)-to-(\w+)/i.match(line).to_a
+
+    mapping = Mapping.new(source, destination)
+    almanac << mapping
+
+    loop do
+      line = file.readline.chomp
+      break if /\A\s*\z/.match?(line) || file.eof?
+
+      dest_start, src_start, length = line.split(/\s+/).map(&:to_i)
+      mapping.add_range(dest_start, src_start, length)
+    end
+  end
+end
+
+locations = seeds.map do |seed|
+  almanac.reduce(seed) { |s, mapping| mapping.map(s) }
+end
+
+log_path = Pathname.new(File.expand_path('rlog1.txt'))
+log_file = File.open(log_path, 'w+')
+
+seeds.each_with_index do |seed, index|
+  log_file.puts "seed: #{seed}, location: #{locations[index]}"
+end
+
 binding.pry
+puts locations.min
 
-maps = []
-
-chunks[1..].each do |chunk|
-  source, destination = chunk[0].split[0].split('-to-')
-  map = Map.new(source, destination)
-  maps << map
-  chunk[1..].each do |line|
-    map.add_range(*line.split(' ').map(&:to_i))
-  end
-end
-
-# seed_to_soil = maps.find { |map| map.source_type == 'seed' && map.dest_type == 'soil' }
-# soil_to_fertilizer = maps.find { |map| map.source_type == 'soil' && map.dest_type == 'fertilizer' }
-# fertilizer_to_water = maps.find { |map| map.source_type == 'fertilizer' && map.dest_type == 'water' }
-# water_to_light = maps.find { |map| map.source_type == 'water' && map.dest_type == 'light' }
-# light_to_temperature = maps.find { |map| map.source_type == 'light' && map.dest_type == 'temperature' }
-# temperature_to_humidity = maps.find { |map| map.source_type == 'temperature' && map.dest_type == 'humidity' }
-# humidity_to_location = maps.find { |map| map.source_type == 'humidity' && map.dest_type == 'location' }
-
-def get_location(seednum, maps)
-  maps.reduce(seednum) do |memo, map|
-    map.lookup(memo)
-  end
-end
-
-seed_map = {}
-
-seeds.each do |seed|
-  seed_map[seed] = get_location(seed, maps)
-end
-
-seed_map.each do |key, val|
-  log_file.puts "seed: #{key}, soil: #{val}"
-end
-
-
-
-puts(seeds.min { |seed| get_location(seed, maps) })
-
-# 43713320689 too high!
-# 28965817 too low!
+# expect 525792406
