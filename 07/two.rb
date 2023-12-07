8dff077e99ca540d8037d40f72bb07855e2ac3c5
Garrett Bodley <garrett.bodley@gmail.com>
2023-12-07 11:28:06 -0500

    Code cleanup

--- 07/two.rb
+++ 07/two.rb
@@ -17,27 +17,7 @@ else
   LOG_PATH = FILE_PATH.parent.join('log_input2.txt')
 end
 
-# Five of a kind, where all five cards have the same label: AAAAA
-# 7
-#
-# Four of a kind, where four cards have the same label and one card has a different label: AA8AA
-# 6
-#
-# Full house, where three cards have the same label, and the remaining two cards share a different label: 23332
-# 5
-#
-# Three of a kind, where three cards have the same label, and the remaining two cards are each different from any other card in the hand: TTT98
-# 4
-#
-# Two pair, where two cards share one label, two other cards share a second label, and the remaining card has a third label: 23432
-# 3
-#
-# One pair, where two cards share one label, and the other three cards have a different label from the pair and each other: A23A4
-# 2
-#
-# High card, where all cards' labels are distinct: 23456
-# 1
-
+# Represents a hand of cards
 class Hand
   attr_reader :cards, :bid, :strength, :joker_count
 
@@ -65,9 +45,8 @@ class Hand
     score_cards
   end
 
-  def score_cards
-
-
+  # rubocop plz shut up
+  def score_cards # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/MethodLength, Metrics/PerceivedComplexity
     map = {}
     map['J'] = 0
     @cards.each_char do |card|
@@ -81,7 +60,6 @@ class Hand
     sorted = map.to_a.sort { |a, b| b[1] - a[1] }.reject { |card| card[0] == 'J' }
     @joker_count = map['J']
 
-    # binding.pry if sorted.length == 1
     value = sorted.empty? ? @joker_count : sorted[0][1] + @joker_count
 
     case value
@@ -98,16 +76,15 @@ class Hand
     end
   end
 
-  def self.all
+  def self.all # rubocop:disable Style/TrivialAccessors
     @all
   end
 
-  def self.sort_hands
+  def self.sort_hands # rubocop:disable Metrics/MethodLength
     @all.sort! do |a, b|
       comparison = a.strength - b.strength
 
-      if comparison == 0
-        comparison = 0
+      if comparison.zero?
         5.times do |i|
           a_strength = @cardmap[a.cards[i]]
           b_strength = @cardmap[b.cards[i]]
@@ -117,6 +94,7 @@ class Hand
           break
         end
       end
+
       comparison
     end
   end
@@ -132,7 +110,6 @@ end
 Hand.sort_hands
 
 winnings = Hand.all.each_with_index.reduce(0) do |accum, (hand, index)|
-  # binding.pry
   accum + (hand.bid * (index + 1))
 end
 
