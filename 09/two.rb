1490cae90c4c6119d46e27c4e53486dce4737c60
Garrett Bodley <garrett.bodley@gmail.com>
2023-12-09 11:22:34 -0500

    Day 9 Part 2 Solution

---
+++ 09/two.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require 'pathname'
+require 'pry-nav'
+
+arg = ARGV.shift
+
+case arg
+when /input|input.txt/
+  FILE_PATH = Pathname.new(File.expand_path('input.txt'))
+when /test|test.txt/
+  FILE_PATH = Pathname.new(File.expand_path('test.txt'))
+else
+  raise ArgumentError, "Expects 'input', 'test' as command line argument"
+end
+
+lines = File.open(FILE_PATH, File::RDONLY).readlines(chomp: true).map { |line| line.split(/\s+/).map(&:to_i) }
+
+def extrapolate(set)
+  sets = [set]
+
+  until sets[-1].all?(&:zero?)
+    cur = sets[-1]
+    new = []
+    cur.each_with_index do |val, i|
+      break if cur[i + 1].nil?
+
+      new << cur[i + 1] - val
+    end
+    sets << new
+  end
+  sets.reverse!.each_with_index do |set, i|
+    break if sets[i + 1].nil?
+
+    sets[i + 1].unshift(sets[i + 1][0] - set[0])
+  end
+  sets[-1][0]
+end
+
+res = lines.reduce(0) { |accum, line| accum + extrapolate(line) }
+
+puts res
+
+# expects 993
