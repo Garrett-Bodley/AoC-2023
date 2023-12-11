11cfb98fa4854a6004707616eccdb01216efd288
Garrett Bodley <garrett.bodley@gmail.com>
2023-12-11 01:23:42 -0500

    Write code to expand empty rows/cols

--- 11/one.rb
+++ 11/one.rb
@@ -14,4 +14,46 @@ else
   raise ArgumentError, "Expects 'input' or 'test' as command line argument"
 end
 
-lines = File.open(FILE_PATH, File::RDONLY).readlines(chomp: true)
+lines = File.open(FILE_PATH, File::RDONLY).readlines(chomp: true).map { |l| l.split('') }
+# binding.pry
+empty_rows = []
+
+lines.each_with_index do |line, index|
+  empty_rows << index unless line.any? { |char| char == '#' }
+end
+
+empty_cols = []
+
+(0...lines[0].length).each do |x|
+  empty = true
+  (0...lines.length).each do |y|
+    if lines[y][x] == '#'
+      empty = false
+      break
+    end
+  end
+  empty_cols << x if empty
+end
+
+# binding.pry
+
+expand_path = FILE_PATH.parent.join("exp_#{FILE_PATH.basename('.*')}.txt")
+expand_file = File.open(expand_path, 'w+')
+
+offset = 0
+empty_rows.each do |idx|
+  lines.insert(idx + offset, Array.new(lines[0].length, '.'))
+  offset += 1
+end
+
+offset = 0
+empty_cols.each do |idx|
+  lines.each do |line|
+    line.insert(idx + offset, '.')
+  end
+  offset += 1
+end
+
+lines.each { |line| expand_file.puts line.join('') }
+
+# binding.pry
