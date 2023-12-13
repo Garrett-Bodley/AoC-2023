a0d085f1a3306b83f4693bd965427d8cc4a96c97
Garrett Bodley <garrett.bodley@gmail.com>
2023-12-12 19:02:03 -0500

    Code cleanup Day 12 Part 1

--- 12/one.rb
+++ 12/one.rb
@@ -21,52 +21,9 @@ data = lines.map do |line|
   [springs.split(''), nums.split(',').map(&:to_i)]
 end
 
-# binding.pry
-# what the hell is this problem asking?
+# attr = %i[start length line]
 
-# given a pattern of ?###???????? 3,2,1
-# how do I figure out which chars satisfy the condition
-# 3, 2, 1
-
-# this seems like a brute force algorithm?
-# how to do fast??
-
-# given first num
-# is there a wildcard spot large enough?
-# is there enough space in wildcard chunk
-
-# so i'm making permutations of the wild card spots?
-# it feels like I need to know where the springs are
-
-# can I reduce the number of test cases to solve
-
-attr = %i[start length line]
-
-Wildcard = Struct.new(*attr)
-
-# def valid?(line, arg_pattern)
-#   pattern = [*arg_pattern]
-
-#   i = 0
-#   while i < line.length
-#     return true if pattern.empty?
-
-#     i += 1 while line[i] != '#' && i < line.length
-#     return false unless i < line.length
-
-#     count = 0
-#     group = pattern.shift
-
-#     while line[i] == '#' && i < line.length
-#       count += 1
-#       i += 1
-#     end
-#     return false if count != group
-
-#     i += 1
-#   end
-#   pattern.empty? ? true : false
-# end
+# Wildcard = Struct.new(*attr)
 
 def valid?(line, pattern)
   groups = line.join('').enum_for(:scan, /#+/).map { Regexp.last_match }
@@ -76,21 +33,7 @@ def valid?(line, pattern)
     group = groups[i]
     return false if group[0].length != group_size
   end
-  return true
-end
-
-
-# what am I doing???
-# I have no idea what I'm doing
-def permutations(line, pattern)
-  wildcards = line.join('').enum_for(:scan, /\?+/).map { Regexp.last_match }
-                  .map { |el| Wildcard.new(el.begin(0), el[0].length, line) }
-
-end
-
-def find_wildcards(line)
-  wildcards = line.join('').enum_for(:scan, /\?+/).map { Regexp.last_match }
-                  .map { |el| Wildcard.new(el.begin(0), el[0].length, line) }
+  true
 end
 
 def brute_force(line, i)
@@ -112,36 +55,6 @@ def brute_force(line, i)
   combinations
 end
 
-# line, pattern = data.last
-# options = brute_force(line, 0)
-
-# valid_options = %w[
-#   .###.##.#...
-#   .###.##..#..
-#   .###.##...#.
-#   .###.##....#
-#   .###..##.#..
-#   .###..##..#.
-#   .###..##...#
-#   .###...##.#.
-#   .###...##..#
-#   .###....##.#
-# ]
-# wrong = [
-#   ['.', '#', '#', '#', '.', '.', '#', '#', '.', '#', '.', '#'],
-#   ['.', '#', '#', '#', '.', '#', '#', '.', '.', '#', '.', '#'],
-#   ['.', '#', '#', '#', '.', '#', '#', '.', '#', '.', '.', '#'],
-#   ['.', '#', '#', '#', '.', '#', '#', '.', '#', '.', '#', '.'],
-#   ['.', '#', '#', '#', '.', '#', '#', '.', '#', '.', '#', '#']
-# ]
-
-# wrong.map! { |line| line.join('') }
-
-# valid = options.filter do |option|
-#   valid?(option, pattern)
-# end
-# binding.pry
-
 res = data.reduce(0) do |sum, (line, pattern)|
   options = brute_force(line, 0)
   valid = options.filter do |option|
@@ -153,6 +66,4 @@ end
 
 puts res
 
-# data.each do |line, pattern|
-#   puts valid?(line, pattern)
-# end
+# expects 7843
