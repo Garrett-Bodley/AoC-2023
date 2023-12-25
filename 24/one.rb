31bd45211c739dbc3c10a809c6b458070bc7fd09
Garrett Bodley <garrett.bodley@gmail.com>
2023-12-25 00:25:34 -0500

    Solution Day 24 Part 1.

--- 24/one.rb
+++ 24/one.rb
@@ -9,8 +9,10 @@ arg = ARGV.shift
 case arg
 when /input|input.txt/
   FILE_PATH = Pathname.new(File.expand_path('input.txt'))
+  RANGE = (200_000_000_000_000..400_000_000_000_000).freeze
 when /test|test.txt/
   FILE_PATH = Pathname.new(File.expand_path('test.txt'))
+  RANGE = (7..27).freeze
 else
   raise ArgumentError, "Expects 'input' or 'test' as command line argument"
 end
@@ -19,100 +21,53 @@ lines = File.open(FILE_PATH, File::RDONLY).readlines(chomp: true)
 
 # Solves Day 24 Part 1
 class Solver
-
   Hail = Struct.new(*%i[x y z vx vy vz]) do
     INVALID_PATH = [-Float::INFINITY..-Float::INFINITY] * 2 # rubocop:disable Lint/ConstantDefinitionInBlock
 
-    def calc_path(xy_range)
-      # what happens to these equations if x < xy_range.begin && vx.negative?
-      # I have to figure out what I'm going to return from this function lol
-
-      # am I moving left or right?
-      # right now I'm always assuming I"m moving left
-      # what if I'm entering from the right to the left
-      # what if I'm entering from the top to the bottom?
-      # oh no
-      # binding.pry
-      # return INVALID_PATH if x < xy_range.begin && vx <= 0
-      # return INVALID_PATH if x > xy_range.end && vx >= 0
-      # return INVALID_PATH if y < xy_range.begin && vy <= 0
-      # return INVALID_PATH if y > xy_range.end && vy >= 0
-
-      # if vx <= 0
-      #   x_path = x_path_left(xy_range)
-      # else
-      #   x_path = x_path_right(xy_range)
-      # end
-      # binding.pry
-      to_xstart = time_to_target(x, xy_range.begin, vx)
-      to_xend = time_to_target(x, xy_range.end, vx)
-
-      to_ystart = time_to_target(y, xy_range.begin, vy)
-      to_yend = time_to_target(y, xy_range.end, vy)
-
-      # binding.pry if to_xstart > to_xend || to_ystart > to_yend
-      # return INVALID_PATH if to_xstart > to_xend || to_ystart > to_yend
-
-      # THIS IS ALL TIME STUFF
-      x_min_t = [to_xstart, to_xend].min
-      x_max_t = [to_xstart, to_xend].max
-
-      y_min_t = [to_ystart, to_yend].min
-      y_max_t = [to_ystart, to_yend].max
-
-      # Find time frame within given xy range
-      time_frame = [x_min_t, y_min_t].max..[x_max_t, y_max_t].min
-
-      x_start = x + vx * time_frame.begin
-      x_end = x + vx * time_frame.end
-      # x_range = x_start..x_end
-
-      # Hacky and Bad
-      x_range = [x_start, x_end].min..[x_start, x_end].max
-
-      y_start = y + vy * time_frame.begin
-      y_end = y + vy * time_frame.end
-      # y_range = y_start..y_end
-
-      y_range = [y_start, y_end].min..[y_start, y_end].max
-
-      binding.pry unless r_overlap?(x_range, xy_range) && r_overlap?(y_range, xy_range)
-      return INVALID_PATH unless r_overlap?(x_range, xy_range) && r_overlap?(y_range, xy_range)
-
-      [x_range, y_range]
+    def slope
+      vy / vx
     end
 
-    def time_to_target(start, target, vector)
-      return 0 if start == target
+    def y_intercept
+      # mx + b = y
+      # b = y - mx
+      y - slope * x
+    end
 
-      if start < target && vector > 0 || start > target && vector < 0
-        # positive time
-        t = (target - start) / vector
-      elsif start < target && vector < 0 || start > target && vector > 0
-        # negative time
-        t = -(target - start / vector)
-      elsif start > target && vector == 0
-        -Float::INFINITY
-      elsif start < target && vector == 0
-        Float::INFINITY
+    def intersection(h2)
+      if parallel?(h2)
+        return true if y_intercept == h2.y_intercept # rubocop:disable Layout/EmptyLineAfterGuardClause
+        return false
       end
+
+      x_collide = x_intersection(h2)
+      y1_collide = y_at_x(x_collide)
+      y2_collide = h2.y_at_x(x_collide)
+
+      # not equivalent bc of float value drift
+      # will this bite me in the but later?
+      # binding.pry if y1_collide != y2_collide
+
+      [x_collide, y1_collide]
     end
 
-    # def x_path_left
+    def time_at_x(arg_x)
 
-    # end
+      (arg_x - x) / vx
+    end
 
-    # def x_path_right
+    def parallel?(h2)
+      slope == h2.slope
+    end
 
-    # end
+    def x_intersection(h2)
+      (h2.y_intercept - y_intercept) / (slope - h2.slope)
+    end
 
-    # def y_path_down
-
-    # end
-
-    # def y_path_up
-
-    # end
+    def y_at_x(a_x)
+      # y = mx + b
+      slope * a_x + y_intercept
+    end
 
     def r_overlap?(r1, r2) # rubocop:disable Naming/MethodParameterName
       r1.end >= r2.begin && r2.end >= r1.begin
@@ -134,47 +89,39 @@ class Solver
   end
 
   def solve
-    paths = @hail.map { |h| [h, h.calc_path(@xy_range)] }
-                 .reject { _1[1].include?([-Float::INFINITY..-Float::INFINITY] * 2) }
+    collisions = []
 
-    binding.pry unless paths.select { _1.include?([-Float::INFINITY..-Float::INFINITY] * 2) }.empty?
+    (0...@hail.length - 1).each do |i|
+      h1 = @hail[i]
+      ((i + 1)...@hail.length).each do |j|
+        h2 = @hail[j]
+        intersection = h1.intersection(h2)
+        next if intersection == false
 
-    collisions = Set.new
+        # problem at 18,19 and 20,25
 
-    (0...paths.length - 1).each do |i|
-      h, (x_range, y_range) = paths[i]
-      ((i + 1)...paths.length).each do |j|
-        cmp_h, (cmp_x_range, cmp_y_range) = paths[j]
-        binding.pry if cmp_h.x == 12
-
-        if r_overlap?(x_range, cmp_x_range) && r_overlap?(y_range, cmp_y_range)
-          collisions << h
-          collisions << cmp_h
+        if intersection == true
+          [h1, h2].each { collisions << _1 }
+          next
         end
+        h1_time = h1.time_at_x(intersection[0])
+        h2_time = h2.time_at_x(intersection[0])
+
+        collisions << [h1, h2] if intersection.all? { @xy_range.cover?(_1) } && h1_time > 0 && h2_time > 0
       end
     end
 
-    binding.pry
-    # paths.map do |path|
-    #   h, (x_range, y_range) = path
-    #   others = paths.reject { _1 == path }
-
-
-
-    # end
+    puts collisions.count
   end
 
   def r_overlap?(r1, r2) # rubocop:disable Naming/MethodParameterName
     r1.end >= r2.begin && r2.end >= r1.begin
   end
-
-
 end
 
-# xy_range = 200_000_000_000_000..400_000_000_000_000
-test_range = 7..27
-
-s = Solver.new(lines, test_range)
+s = Solver.new(lines, RANGE)
 s.solve
+
+# expects 26657
 
 # rubocop:enable Style/NumericPredicate
